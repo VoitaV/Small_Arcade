59587fedbccb745a8219ff994cbf9702e3ad0df7
Valentin Voitovich <v.v.voitovich+git@gmail.com>
2023-11-29 18:27:34 +0300

    Add goblin, move problen

--- main.py
+++ main.py
@@ -64,18 +64,73 @@ class projectile(object):
         self.vel = 8 * facing
 
     def draw(self, win):
-        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
+class enemy(object):
+    walkRight = [
+        pygame.image.load(os.path.join('Game', 'R1E.png')), pygame.image.load(os.path.join('Game', 'R2E.png')),
+        pygame.image.load(os.path.join('Game', 'R3E.png')), pygame.image.load(os.path.join('Game', 'R4E.png')),
+        pygame.image.load(os.path.join('Game', 'R5E.png')), pygame.image.load(os.path.join('Game', 'R6E.png')),
+        pygame.image.load(os.path.join('Game', 'R7E.png')), pygame.image.load(os.path.join('Game', 'R8E.png')),
+        pygame.image.load(os.path.join('Game', 'R9E.png')), pygame.image.load(os.path.join('Game', 'R10E.png')),
+        pygame.image.load(os.path.join('Game', 'R11E.png'))]
+    walkLeft = [
+        pygame.image.load(os.path.join('Game', 'L1E.png')), pygame.image.load(os.path.join('Game', 'L2E.png')),
+        pygame.image.load(os.path.join('Game', 'L3E.png')), pygame.image.load(os.path.join('Game', 'L4E.png')),
+        pygame.image.load(os.path.join('Game', 'L5E.png')), pygame.image.load(os.path.join('Game', 'L6E.png')),
+        pygame.image.load(os.path.join('Game', 'L7E.png')), pygame.image.load(os.path.join('Game', 'L8E.png')),
+        pygame.image.load(os.path.join('Game', 'L9E.png')), pygame.image.load(os.path.join('Game', 'L10E.png')),
+        pygame.image.load(os.path.join('Game', 'L11E.png'))]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.x]
+        self.walkCount = 0
+        self.vel = 3
+
+    def draw(self, win):
+        self.move()
+        if self.walkCount + 1 >= 33:  # end of sprite loop
+            self.walkCount = 0
+        if self.vel > 0:
+            win.blit(self.walkRight[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(self.walkLeft[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+
+    def move(self):
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+
 
 def redrawGameWindow():
 
     win.blit(bg, (0, 0))  # draw our background image at (0,0)
     man.draw(win)
+    goblin.draw(win)
     for bullet in bullets:
         bullet.draw(win)
     pygame.display.update()
 
 
-man = player(300, 410, 64, 64)
+man = player(200, 410, 64, 64)
+goblin = enemy(100, 410, 64, 64, 450)
 bullets = []
 # Main loop
 run = True
@@ -86,7 +141,7 @@ while run:
             run = False
 
     for bullet in bullets:
-        if bullet.x < 500 and bullet.x > 0:
+        if 500 > bullet.x > 0:
             bullet.x += bullet.vel
         else:
             bullets.pop(bullets.index(bullet))
@@ -98,7 +153,7 @@ while run:
         else:
             facing = 1
         if len(bullets) < 5:
-            bullets.append(projectile(round(man.x+man.width//2), round(man.y+man.height//2), 6, (0,0,0), facing))
+            bullets.append(projectile(round(man.x+man.width//2), round(man.y+man.height//2), 6, (0, 0, 0), facing))
     if keys[pygame.K_LEFT] and man.x - man.vel >= 0:  # move left
         man.x -= man.vel
         man.left = True
